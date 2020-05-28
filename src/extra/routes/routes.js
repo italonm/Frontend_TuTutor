177e9f3bb5dba91bdd93bb625ccbf31687e58ab0
italo <gez.italorafael@gmail.com>
2020-05-27 19:37:38 -0500

    hehe

--- src/extra/routes/routes.js
+++ src/extra/routes/routes.js
@@ -4,6 +4,7 @@ import Configuración from '../../Usuarios/Administrador/Configuración/Configur
 /* import Errores from '../../Usuarios/Administrador/Errores' */
 import Académicas from '../../Usuarios/Administrador/Unidades académicas/Unidades académicas'
 import Usuarios from '../../Usuarios/Administrador/Usuarios/Usuarios'
+import Coordinadores from '../../Usuarios/Administrador/Coordinadores/Coordinadores.vue'
 //COMPONENTES DE COORDINADOR
 import Alumnos from '../../Usuarios/Coordinador/Alumnos/Alumnos'
 import Tutores from '../../Usuarios/Coordinador/Tutores/Tutores'
@@ -18,95 +19,98 @@ import Apoyo from '../../Usuarios/Coordinador/Unidades de apoyo/Unidades de apoy
 //COMPONENTE BIENVENIDOS
 import Bienvenido from '../../Usuarios/Welcome'
 
-export const rutitas ={
-  admin:[    
-    {
-      path: 'Bienvenido',
-      name: 'Bienvenido',
-      component: Bienvenido
-    },
-    {
-      path: 'Configuración',
-      name: 'Configuración',
-      component: Configuración
-    },
-    {
-      path: 'Unidades Académicas',
-      name: 'Unidades Académicas',
-      component: Académicas
-    },
-    {
-      path: 'Usuarios',
-      name: 'Usuarios',
-      component: Usuarios
-    }/* ,
-    {
-      path: 'Auditoría',
-      name: 'Auditoría',
-      component: Auditoría
-    },
-    {
-      path: 'Errores',
-      name: 'Errores',
-      component: Errores
-    } */
-  ],
-  coordi:[         
-    {
-      path: 'Bienvenido',
-      name: 'Bienvenido',
-      component: Bienvenido
-    },
-    {
-      path: 'Miembros',
-      name: 'Miembros',
-      component: {
-      render (c) { return c('router-view') }
-      },
-      children: [
-        {
-          path: 'Alumnos',
-          name: 'Alumnos',
-          component: Alumnos
+export const rutitas = {
+    admin: [{
+            path: 'Bienvenido',
+            name: 'Bienvenido',
+            component: Bienvenido
         },
         {
-          path: 'Tutores',
-          name: 'Tutores',
-          component: Tutores
+            path: 'Configuración',
+            name: 'Configuración',
+            component: Configuración
         },
         {
-          path: 'Soporte',
-          name: 'Soporte',
-          component: Soporte
+            path: 'Unidades Académicas',
+            name: 'Unidades Académicas',
+            component: Académicas
         },
         {
-          path: 'Unidades de apoyo',
-          name: 'Unidades de apoyo',
-          component: Apoyo
+            path: 'Usuarios',
+            name: 'Usuarios',
+            component: Usuarios
+        },
+        {
+            path: 'Coordinadores',
+            name: 'Coordinadores',
+            component: Coordinadores
+        },
+        /* ,
+        {
+          path: 'Auditoría',
+          name: 'Auditoría',
+          component: Auditoría
+        },
+        {
+          path: 'Errores',
+          name: 'Errores',
+          component: Errores
+        } */
+    ],
+    coordi: [{
+            path: 'Bienvenido',
+            name: 'Bienvenido',
+            component: Bienvenido
+        },
+        {
+            path: 'Miembros',
+            name: 'Miembros',
+            component: {
+                render(c) { return c('router-view') }
+            },
+            children: [{
+                    path: 'Alumnos',
+                    name: 'Alumnos',
+                    component: Alumnos
+                },
+                {
+                    path: 'Tutores',
+                    name: 'Tutores',
+                    component: Tutores
+                },
+                {
+                    path: 'Soporte',
+                    name: 'Soporte',
+                    component: Soporte
+                },
+                {
+                    path: 'Unidades de apoyo',
+                    name: 'Unidades de apoyo',
+                    component: Apoyo
+                }
+            ]
+        },
+        {
+            path: 'Tipo de tutoría',
+            name: 'Tipo de tutoría',
+            component: TipoTutoria
+        },
+        {
+            path: 'Tutorías Activas',
+            name: 'Tutorías Activas',
+            component: Tutorías
+        },
+        /*{
+          path: 'Reportes',
+          name: 'Reportes',
+          component: Reportes
+        }, */
+        {
+            path: 'Solicitudes',
+            name: 'Solicitudes',
+            component: Solicitudes
         }
-      ]
-    },    
-    {
-      path: 'Tipo de tutoría',
-      name: 'Tipo de tutoría',
-      component: TipoTutoria
-    },
-    {
-      path: 'Tutorías Activas',
-      name: 'Tutorías Activas',
-      component: Tutorías
-    },
-    /*{
-      path: 'Reportes',
-      name: 'Reportes',
-      component: Reportes
-    }, */
-    {
-      path: 'Solicitudes',
-      name: 'Solicitudes',
-      component: Solicitudes
-    }
-  ]        
+    ]
 }
 
 import Vue from 'vue'
@@ -116,20 +120,19 @@ import Router from 'vue-router'
 import Login from '../../pages/Login/Login.vue'
 
 Vue.use(Router)
-export const RutaLogin =[
-  {
-    name:'Inicio',
-    path: '',
-    redirect:'/TuTutor?#',
-  },
-  {
-    name:'Login',
-    path: '/TuTutor?#',
-    component: Login
-  }
+export const RutaLogin = [{
+        name: 'Inicio',
+        path: '',
+        redirect: '/TuTutor?#',
+    },
+    {
+        name: 'Login',
+        path: '/TuTutor?#',
+        component: Login
+    }
 ]
 
 export default new Router({
-  mode: 'history', // https://router.vuejs.org/api/#mode
-  routes: RutaLogin,
+    mode: 'history', // https://router.vuejs.org/api/#mode
+    routes: RutaLogin,
 })
