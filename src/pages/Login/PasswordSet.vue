705b20cd9fc675cd11544cdbb76196cc529e7534
Melvin <m.gomezg@pucp.edu.pe>
2020-06-11 14:01:35 -0500

    Doble login fixed

---
+++ src/pages/Login/PasswordSet.vue
@@ -0,0 +1,19 @@
+
+<template lang="pug">
+    .container(onclick)
+    .top
+    .bottom
+    .center
+        h2 Please Sign In
+        input(type="email" placeholder="email")
+        input(type="password" placeholder="password")
+        h2 &nbsp;
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    @import "./Password.scss";
+</style>
