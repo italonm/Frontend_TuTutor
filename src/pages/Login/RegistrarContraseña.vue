49989ef9a1f511157bedbde13a0741925af0bf8b
Melvin <m.gomezg@pucp.edu.pe>
2020-06-12 14:05:18 -0500

    Antes del merge

---
+++ src/pages/Login/RegistrarContraseña.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="nav containerPrincipal">
+        <div class='column'>
+            <h1 class='header'>Registrar</h1>
+            <p class='subheader'>Ingrese su contraseña</p> 
+            <div class="icon1">
+                <button class="effect1" href="#">
+                    Enviar
+                    <span class="bg"></span>
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    @import "./RegContra.css";       
+</style>
+<style lang="scss">
+    @import "./Password.scss";   
+</style>
