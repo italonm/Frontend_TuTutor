ee79028fc18445dccd46293643a7e89f73904cf6
Melvin <m.gomezg@pucp.edu.pe>
2020-06-11 23:46:00 -0500

    Nuevo ruteo de Alumno

--- src/pages/Login/PasswordSet.vue
+++ src/pages/Login/PasswordSet.vue
@@ -1,13 +1,13 @@
 
 <template lang="pug">
     .container(onclick)
-    .top
-    .bottom
-    .center
-        h2 Please Sign In
-        input(type="email" placeholder="email")
-        input(type="password" placeholder="password")
-        h2 &nbsp;
+        .top
+        .bottom
+        .center
+            h2 Please Sign In
+            input(type="email", placeholder="email")
+            input(type="password", placeholder="password")
+            h2 &nbsp;
 </template>
 <script>
 export default {
