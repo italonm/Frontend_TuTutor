4b700ab71c9e8b4193f99526fc877f211d43d922
Luiggi <a20152396@pucp.pe>
2020-06-18 00:35:34 -0500

    Corrección Tipo de Tutoría

--- src/Usuarios/Coordinador/Tipos de tutoría/TipoTutoria.vue
+++ src/Usuarios/Coordinador/Tipos de tutoría/TipoTutoria.vue
@@ -76,7 +76,7 @@ export default {
         { text: "Nombre", value: "tt_name" },
         { text: "Descripción", value: "tt_description" },
         { text: "Cantidad de alumnos", value: "tt_quantity" },
-        { text: "Periodicidad", value: "tt_periodicity" },
+        { text: "Duración", value: "tt_periodicity" },
         { text: "¿Es obligatorio?", value: "tt_isrequired" },
         { text: "¿El tutor es asignado?", value: "tt_assigned" },
         { text: "¿El tutor es fijo?", value: "tt_permanent" },
